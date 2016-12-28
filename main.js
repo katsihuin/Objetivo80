80843ae7676e0b89ef4f5f78f6dc4bac0d9a3258
katsihuin <katsihuin@gmail.com>
2016-12-27 22:24:33 -0500

    js y html terminado

--- main.js
+++ main.js
@@ -2,14 +2,15 @@
 
 function calcular () 
 {
-	var puntosSquad = document.getElementById("puntosSquad").value;
-	var puntosMaximo = document.getElementById("puntosMaximo").value;
-	var porcentajeSquad = (100*puntosSquad)/puntosMaximo;
+	var puntosSquad = document.getElementById('puntosSquad').value;
+	var puntosMaximo = document.getElementById('puntosMaximo').value;
+	var porcentajeSquad = 100*(puntosSquad/puntosMaximo);
 
-	if (porcentajeSquad >= 80)
-	{
-		document.getElementById("salida").innerHTML="Muy bien Squad!";
-	} else {
-		document.getElementById("salida").innerHTML="Pueden hacerlo mejor.";
+	if (puntosSquad == "" || puntosMaximo == ""){
+        document.getElementById("salida").innerHTML = "<div class='alert'>Ingrese valores</div>";
+    } else if (porcentajeSquad >= 80) {
+		document.getElementById('salida').innerHTML = "<div class='alert'>Muy bien Squad!</div>";
+    } else { 
+		document.getElementById('salida').innerHTML = "<div class='alert'>Pueden hacerlo mejor.</div>";
 	}
 }
