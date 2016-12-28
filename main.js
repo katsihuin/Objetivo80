c50185ae027e97386f0dbd0af86a20c82e8665ca
Katherin Sihuin <katsihuin@gmail.com>
2016-12-28 10:17:41 -0500

    agregando link de github

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ function calcular ()
 	var puntosSquad = document.getElementById('puntosSquad').value;
 	var puntosMaximo = document.getElementById('puntosMaximo').value;
 	var porcentajeSquad = 100*(puntosSquad/puntosMaximo);
+	var salida = document.getElementById("salida");
 
 	if (puntosSquad == "" || puntosMaximo == ""){
+
         document.getElementById("salida").innerHTML = "<div class='alert'>Ingrese valores</div>";
-    } else if (porcentajeSquad >= 80) {
-		document.getElementById('salida').innerHTML = "<div class='alert'>Muy bien Squad!</div>";
+    } 
+    if (porcentajeSquad >= 80) {
+    	salida.innerHTML = "<div class='alert'>Muy bien Squad!</div>";
     } else { 
 		document.getElementById('salida').innerHTML = "<div class='alert'>Pueden hacerlo mejor.</div>";
 	}
